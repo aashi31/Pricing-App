a2fca4bda8a79fcf9b2e000981b81e0b84f440ab
Aashi Singh <82797090+aashi31@users.noreply.github.com>
2023-07-28 19:32:30 +0530

    Update admin.py

--- pricing_app/admin.py
+++ pricing_app/admin.py
@@ -1,4 +1,3 @@
-# pricing_app/admin.py
 from django.contrib import admin
 from django.contrib.admin.models import LogEntry, ADDITION, CHANGE
 from .models import PricingConfiguration, ConfigurationChangeLog
@@ -8,7 +7,6 @@ class PricingConfigurationAdmin(admin.ModelAdmin):
     form = PricingConfigurationForm
 
     def save_model(self, request, obj, form, change):
-        # Save the user making the change in the form's save method
         obj = form.save(commit=True, user=request.user)
         super().save_model(request, obj, form, change)
 
