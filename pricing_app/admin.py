feee35561041653e0be915b3b839c1b8fed90a5e
Aashi Singh <82797090+aashi31@users.noreply.github.com>
2023-08-08 20:32:44 +0530

    Update admin.py
    I have added generalized "Export selected records to CSV" action available in the Django admin interface for all model classes within the specified app. When selected, it will export the selected records to a CSV file containing the model fields as columns

--- pricing_app/admin.py
+++ pricing_app/admin.py
@@ -1,12 +1,17 @@
+
 from django.contrib import admin
 from django.contrib.admin.models import LogEntry, ADDITION, CHANGE
 from .models import PricingConfiguration, ConfigurationChangeLog
 from .forms import PricingConfigurationForm
+from .actions import export_to_csv
 
 class PricingConfigurationAdmin(admin.ModelAdmin):
     form = PricingConfigurationForm
+    
+    actions = [export_to_csv]
 
     def save_model(self, request, obj, form, change):
+        # Save the user making the change in the form's save method
         obj = form.save(commit=True, user=request.user)
         super().save_model(request, obj, form, change)
 
